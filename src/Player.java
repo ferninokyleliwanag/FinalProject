ab8c4c23edc83a593faca4026241cb4bcb86a4dd
Fernino Kyle Liwanag <ferninokyleliwanag@gmail.com>
2018-05-23 20:59:32 -0400

    Started adding painting

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Player extends Entity implements IPaint {
+
+    public Player(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void paintImage(Graphics g) {
+    }
+
+}
